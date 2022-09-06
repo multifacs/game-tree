7d02f34d21f1b56c80408021e15f46baf2cbddec
multifacs <kiwi0tails@gmail.com>
2022-09-06 14:13:19 +0300

    feat: generate zero func and print children rows

--- gametree.h
+++ gametree.h
@@ -1,119 +1,158 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 using std::cout;
 using std::setw;
 
 struct treeNode {
-    int state[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    struct treeNode** next = nullptr;
-    int number = 0;
-    int go = 0;
-    int children = 0;
-    bool win = false;
+  int state[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  struct treeNode** next = nullptr;
+  int number = 0;
+  int go = 0;
+  int children = 0;
+  bool win = false;
 
-    void print() {
-        cout << number << "\n";
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                char output = '-';
-                if (state[3 * i + j] == 1) output = 'X';
-                if (state[3 * i + j] == 2) output = 'O';
-                cout << output << " ";
-            }
-            cout << "\n";
-        }
+  void print() {
+    cout << number << "\n";
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        char output = '-';
+        if (state[3 * i + j] == 1) output = 'X';
+        if (state[3 * i + j] == 2) output = 'O';
+        cout << output << " ";
+      }
+      cout << "\n";
     }
+  }
 
-    void printChildren() {
-        for (int c = 0; c < children; c++) {
-            cout << next[c]->number << setw(10);
+  void printChildren(int num = 3) {
+    int part = children / num;
+    int rem = children % num;
+
+    for (int k = 0; k < num; k++) {
+      for (int c = part * k; c < part * (k + 1); c++) {
+        cout << next[c]->number << setw(10);
+      }
+      cout << "\n";
+
+      for (int i = 0; i < 3; i++) {
+        for (int c = part * k; c < part * (k + 1); c++) {
+          for (int j = 0; j < 3; j++) {
+            char output = '-';
+            if (next[c]->state[3 * i + j] == 1) output = 'X';
+            if (next[c]->state[3 * i + j] == 2) output = 'O';
+            cout << output << " ";
+          }
+          cout << setw(5);
         }
         cout << "\n";
-
-        for (int i = 0; i < 3; i++) {
-            for (int c = 0; c < children; c++) {
-                for (int j = 0; j < 3; j++) {
-                    char output = '-';
-                    if (next[c]->state[3 * i + j] == 1) output = 'X';
-                    if (next[c]->state[3 * i + j] == 2) output = 'O';
-                    cout << output << " ";
-                }
-                cout << setw(5);
-            }
-            cout << "\n";
-        }
+      }
+      cout << "\n";
     }
 
-    bool checkWin() {
-        for (int i = 0; i < 3; i++) {
-            int consecutiveMovesX = 0;
-            int consecutiveMovesY = 0;
-            for (int j = 0; j < 3; j++) {
-                consecutiveMovesX += state[i * 3 + j];
-                consecutiveMovesY += state[j * 3 + i];
-            }
-            if (consecutiveMovesX % 3 == 0 || consecutiveMovesY % 3 == 0) {
-                win = true;
-                break;
-            }
-        }
+    for (int c = part * num; c < part * num + rem; c++) {
+      cout << next[c]->number << setw(10);
+    }
+    cout << "\n";
 
-        if (state[0] + state[4] + state[8] % 3 == 0) {
-            win = true;
+    for (int i = 0; i < 3; i++) {
+      for (int c = part * num; c < part * num + rem; c++) {
+        for (int j = 0; j < 3; j++) {
+          char output = '-';
+          if (next[c]->state[3 * i + j] == 1) output = 'X';
+          if (next[c]->state[3 * i + j] == 2) output = 'O';
+          cout << output << " ";
         }
-        if (state[2] + state[4] + state[6] % 3 == 0) {
-            win = true;
-        }
+        cout << setw(5);
+      }
+      // cout << "\n";
+    }
+  }
 
-        return win;
+  bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+      int consecutiveMovesX = 0;
+      int consecutiveMovesY = 0;
+      for (int j = 0; j < 3; j++) {
+        consecutiveMovesX += state[i * 3 + j];
+        consecutiveMovesY += state[j * 3 + i];
+      }
+      if (consecutiveMovesX % 3 == 0 || consecutiveMovesY % 3 == 0) {
+        win = true;
+        break;
+      }
     }
 
-    void generate(int num = 8) {
-        int moves = 0;
-        int moveSpots[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-        for (int spot = 0; spot < 9; spot++) {
-            if (state[spot] == 0) {
-                moveSpots[moves] = spot;
-                // cout << spot << " ";
-                moves += 1;
-            }
-        }
-        // cout << "\n";
-        children = num;
-        if (children > moves) children = moves;
-        if (children) next = new treeNode * [children];
-
-        for (int spot = 0; spot < moves; spot++) {
-            int index = moveSpots[spot];
-
-            next[spot] = new treeNode;
-            std::copy(state, state + 9, next[spot]->state);
-            next[spot]->number = number + spot + 1;
-
-            if (go == 0) {
-                if (spot % 2 == 0) {
-                    next[spot]->state[index] = 1;
-                    next[spot]->go = 1;
-                }
-                if (spot % 2 != 0) {
-                    next[spot]->state[index] = 2;
-                    next[spot]->go = 2;
-                }
-            }
-
-            if (go == 1) {
-                next[spot]->state[index] = 2;
-                next[spot]->go = 2;
-            }
-
-            if (go == 2) {
-                next[spot]->state[index] = 1;
-                next[spot]->go = 1;
-            }
-
-            next[spot]->checkWin();
-        }
+    if (state[0] + state[4] + state[8] % 3 == 0) {
+      win = true;
     }
+    if (state[2] + state[4] + state[6] % 3 == 0) {
+      win = true;
+    }
+
+    return win;
+  }
+
+  void generate(int num = 8) {
+    int moves = 0;
+    int moveSpots[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    if (go == 0) {
+      generateZero(num);
+      return;
+    }
+
+    for (int spot = 0; spot < 9; spot++) {
+      if (state[spot] == 0) {
+        moveSpots[moves] = spot;
+        // cout << spot << " ";
+        moves += 1;
+      }
+    }
+    // cout << "\n";
+    children = num;
+    if (children > moves) children = moves;
+    if (children) next = new treeNode*[children];
+
+    for (int spot = 0; spot < moves; spot++) {
+      int index = moveSpots[spot];
+
+      next[spot] = new treeNode;
+      std::copy(state, state + 9, next[spot]->state);
+      next[spot]->number = number + spot + 1;
+
+      if (go == 1) {
+        next[spot]->state[index] = 2;
+        next[spot]->go = 2;
+      }
+
+      if (go == 2) {
+        next[spot]->state[index] = 1;
+        next[spot]->go = 1;
+      }
+
+      next[spot]->checkWin();
+    }
+  }
+
+  void generateZero(int num = 18) {
+    next = new treeNode*[num];
+    children = num;
+
+    for (int spot = 0; spot < num; spot++) {
+      int index = spot % 9;
+
+      next[spot] = new treeNode;
+      next[spot]->number = number + spot + 1;
+
+      if (spot % 2 == 0) {
+        next[spot]->state[index] = 1;
+        next[spot]->go = 1;
+      }
+      if (spot % 2 != 0) {
+        next[spot]->state[index] = 2;
+        next[spot]->go = 2;
+      }
+    }
+  }
 };
--- main.cpp
+++ main.cpp
@@ -1,54 +1,61 @@
 #include <gtest/gtest.h>
+
 #include "gametree.h"
 
 TEST(GameTree, CREATE_TREE) {
-	treeNode* gameTree = nullptr;
-	ASSERT_NO_THROW(gameTree = new treeNode());
-	ASSERT_EQ(gameTree->go, 0);
+  treeNode* gameTree = nullptr;
+  ASSERT_NO_THROW(gameTree = new treeNode());
+  ASSERT_EQ(gameTree->go, 0);
 }
 
 TEST(GameTree, GENERATE) {
-	treeNode* gameTree = new treeNode();
-	// ASSERT_NO_THROW(gameTree->generate(5));
+  treeNode* gameTree = new treeNode();
+  // ASSERT_NO_THROW(gameTree->generate(5));
 }
 
 TEST(GameTree, WIN) {
-	treeNode* gameTree = new treeNode();
-	gameTree->state[1] = 1;
-	gameTree->go = 1;
-	// gameTree->print();
-	// cout << "\n";
+  treeNode* gameTree = new treeNode();
+  gameTree->state[1] = 1;
+  gameTree->go = 1;
+  // gameTree->print();
+  // cout << "\n";
 
-	gameTree->generate(5);
-	// gameTree->printChildren();
-	treeNode* game1 = gameTree->next[2];
+  gameTree->generate(5);
+  // gameTree->printChildren();
+  treeNode* game1 = gameTree->next[2];
 
-	game1->generate(5);
-	// game1->printChildren();
-	treeNode* game2 = game1->next[4];
+  game1->generate(5);
+  // game1->printChildren();
+  treeNode* game2 = game1->next[4];
 
-	game2->generate(5);
-	// game2->printChildren();
-	treeNode* game3 = game2->next[2];
+  game2->generate(5);
+  // game2->printChildren();
+  treeNode* game3 = game2->next[2];
 
-	game3->generate(5);
-	// game3->printChildren();
-	treeNode* game4 = game3->next[4];
+  game3->generate(5);
+  // game3->printChildren();
+  treeNode* game4 = game3->next[4];
 
-	game4->generate(5);
-	// game4->printChildren();
-	treeNode* game5 = game4->next[2];
+  game4->generate(5);
+  // game4->printChildren();
+  treeNode* game5 = game4->next[2];
 
-	//game1->print();
-	//cout << "\n";
-	//game2->print();
-	//cout << "\n";
-	//game3->print();
-	//cout << "\n";
-	//game4->print();
-	//cout << "\n";
-	//game5->print();
-	//cout << "\n";
+  // game1->print();
+  // cout << "\n";
+  // game2->print();
+  // cout << "\n";
+  // game3->print();
+  // cout << "\n";
+  // game4->print();
+  // cout << "\n";
+  // game5->print();
+  // cout << "\n";
 
-	// cout << game5->win;
+  // cout << game5->win;
+}
+
+TEST(GameTree, GENERATE_ZERO) {
+  treeNode* gameTree = new treeNode();
+  gameTree->generate(18);
+  gameTree->printChildren(6);
 }
